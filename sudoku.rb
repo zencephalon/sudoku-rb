5ca3516980f4f425a2ca90bcc0758679c8df22bb
Matthew Bunday <zenceph@gmail.com>
2015-02-06 10:55:47 -0500

    Refactor init method on cell.

--- sudoku.rb
+++ sudoku.rb
@@ -1,12 +1,12 @@
 class Sudoku
   def initialize(board_str)
-
+    board_str.split.
   end
 end
 
 class Cell
-  def initialize(num = nil)
-    @num = num
+  def initialize(num)
+    @num = num == 0 ? nil : num
     @open = @num ? [] : (1..9).to_a
   end
 end
