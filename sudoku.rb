4c3b177ae434391386cb372849d4ce47b177e824
Matthew Bunday <zenceph@gmail.com>
2015-02-06 10:54:07 -0500

    Add cell class.

--- sudoku.rb
+++ sudoku.rb
@@ -1,5 +1,12 @@
 class Sudoku
-  def initialize
+  def initialize(board_str)
 
+  end
+end
+
+class Cell
+  def initialize(num = nil)
+    @num = num
+    @open = @num ? [] : (1..9).to_a
   end
 end
